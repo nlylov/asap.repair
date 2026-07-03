60a3643e54f642f27626c4ec76692d064d5d84ea
Nikita <nikita@mac-mini-nikita.mynetworksettings.com>
2026-07-03 10:35:25 -0400

    Handle legacy AC redirects in middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -5,6 +5,14 @@ const PRESERVE_PATH_HOSTS = new Set([
     'asap-repair.netlify.app',
 ]);
 
+const LEGACY_PATH_REDIRECTS = new Map([
+    ['/ac_install', '/services/ac-installation-cleaning/'],
+    ['/ac_install/', '/services/ac-installation-cleaning/'],
+    ['/ac-installation-cleaning', '/services/ac-installation-cleaning/'],
+    ['/ac-installation-cleaning/', '/services/ac-installation-cleaning/'],
+    ['/services/ac-installation-cleaning', '/services/ac-installation-cleaning/'],
+]);
+
 function canonicalPathname(pathname) {
     const lastSegment = pathname.split('/').pop() || '';
     const hasFileExtension = lastSegment.includes('.');
@@ -19,11 +27,18 @@ function canonicalPathname(pathname) {
 export async function onRequest(context) {
     const url = new URL(context.request.url);
 
-    if (url.hostname === 'api.asap.repair') {
-        if (url.pathname.startsWith('/api/')) {
-            return context.next();
-        }
+    if (url.hostname === 'api.asap.repair' && url.pathname.startsWith('/api/')) {
+        return context.next();
+    }
 
+    const legacyRedirectPath = LEGACY_PATH_REDIRECTS.get(url.pathname);
+    if (legacyRedirectPath) {
+        url.hostname = CANONICAL_HOST;
+        url.pathname = legacyRedirectPath;
+        return Response.redirect(url.toString(), 301);
+    }
+
+    if (url.hostname === 'api.asap.repair') {
         url.hostname = CANONICAL_HOST;
         url.pathname = canonicalPathname(url.pathname);
         return Response.redirect(url.toString(), 301);
