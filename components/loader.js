9024ddc8e59040e4157e39fb2fcc8f850ffc6acc
Nikita <nikita@mac-mini-nikita.mynetworksettings.com>
2026-07-03 03:25:59 -0400

    Clean up proof content claims

--- components/loader.js
+++ components/loader.js
@@ -6,7 +6,7 @@
 (function () {
     'use strict';
 
-    const ASSET_VERSION = '20260703i';
+    const ASSET_VERSION = '20260703j';
     const versionedAsset = (path) => `${path}?v=${ASSET_VERSION}`;
     const fetchComponent = (path) => fetch(versionedAsset(path));
 
